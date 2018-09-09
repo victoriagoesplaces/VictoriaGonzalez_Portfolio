ae2ed1229fcd03e2cdc14f2913b4e8b69e5dfff4
Victoria Gonzalez <vgonzalez3092@gmail.com>
2018-09-09 19:45:21 -0400

    new portfolio setup

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
-/*----------------------------------------------------*/
-/* Quote Loop
------------------------------------------------------- */
+function loadImage(id, targetId) {
+  // Either an <img>, or an element with a "data-image" attribute
+  var el = document.getElementById(id);
 
-function fade($ele) {
-    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
-        var $next = $(this).next('.quote');
-        fade($next.length > 0 ? $next : $(this).parent().children().first());
-   });
+  // Either the same element as above, or the targetId element
+  var targetEl = targetId ? document.getElementById(targetId) : el;
+
+  // Either the loaded <img>, or the path specified by the "data-image" attribute
+  var imageToLoad;
+
+  if (el.dataset.image) {
+    imageToLoad = el.dataset.image;
+  } else if (typeof el.currentSrc === 'undefined') {
+    imageToLoad = el.src;
+  } else {
+    imageToLoad = el.currentSrc;
+  }
+
+  if (imageToLoad) {
+    var img = new Image();
+    img.src = imageToLoad;
+    img.onload = function() {
+      targetEl.classList.add('is-loaded');
+    };
+  }
 }
-fade($('.quoteLoop > .quote').first());
 
+document.addEventListener('DOMContentLoaded', function() {
+  loadImage('wallpaper');
+  loadImage('pictureImage', 'picture');
+});
 
 /*----------------------------------------------------*/
 /* Navigation
@@ -17,27 +36,27 @@ fade($('.quoteLoop > .quote').first());
 
 $(window).scroll(function() {
 
-    if ($(window).scrollTop() > 300) {
-        $('.main_nav').addClass('sticky');
-    } else {
-        $('.main_nav').removeClass('sticky');
-    }
+  if ($(window).scrollTop() > 300) {
+      $('.main_nav').addClass('sticky');
+  } else {
+      $('.main_nav').removeClass('sticky');
+  }
 });
 
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
-    if ($('.main_nav').hasClass('open-nav')) {
-        $('.main_nav').removeClass('open-nav');
-    } else {
-        $('.main_nav').addClass('open-nav');
-    }
+  if ($('.main_nav').hasClass('open-nav')) {
+      $('.main_nav').removeClass('open-nav');
+  } else {
+      $('.main_nav').addClass('open-nav');
+  }
 });
 
 $('.main_nav li a').click(function() {
-    if ($('.main_nav').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_nav').removeClass('open-nav');
-    }
+  if ($('.main_nav').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.main_nav').removeClass('open-nav');
+  }
 });
 
 
@@ -47,19 +66,19 @@ $('.main_nav li a').click(function() {
 
 jQuery(document).ready(function($) {
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
+ $('.smoothscroll').on('click',function (e) {
+    e.preventDefault();
 
-	    var target = this.hash,
-	    $target = $(target);
+    var target = this.hash,
+    $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-  
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
 });
 
 
